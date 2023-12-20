4a65455a9b78ddee4cb8734a185d70b78c98d2df
Rajarshi Sarkar <143781304+administrator91@users.noreply.github.com>
2023-12-20 10:33:17 +0530

    Update project.js

--- script/project/project.js
+++ script/project/project.js
@@ -18,7 +18,7 @@ var dataJson = [
         "url":"https://github.com/administrator91/Angry_Bird"
     },
     {
-      "img": "",
+      "img": "https://img.freepik.com/free-vector/sticker-design-with-calculator-isolated_1308-60994.jpg?size=626&ext=jpg&ga=GA1.1.1225026954.1700406531&semt=ais",
       "title": "Calculator_Web",
       "paragraph":"This is a Simple Web_Calculator",
       "url":"https://administrator91.github.io/Calculator_web/"
