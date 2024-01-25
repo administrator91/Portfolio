8d5c9ac673b8c075c626da46a5f0df4bedf98d3e
Rajarshi Sarkar <143781304+administrator91@users.noreply.github.com>
2024-01-25 10:04:23 +0530

    Update sc.js

--- sc.js
+++ sc.js
@@ -1,5 +1,5 @@
 function downloadResume() {
-    // You can replace 'path/to/your/resume.pdf' with the actual path to your resume file
+    // path
     const resumePath = 'https://drive.google.com/file/d/1Iusq1ipDRoW99417123AfkATk00X0C5L/view?usp=drivesdk';
 
     // Creating a temporary anchor element to trigger the download
