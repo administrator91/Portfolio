ca7429b66afb243d509b6a5032b0feb364e116e1
Rajarshi Sarkar <143781304+administrator91@users.noreply.github.com>
2024-10-02 01:12:55 +0530

    upload files

---
+++ script/sc.js
@@ -0,0 +1,12 @@
+function downloadResume() {
+    // path
+    const resumePath = 'https://drive.google.com/file/d/1Iusq1ipDRoW99417123AfkATk00X0C5L/view?usp=drivesdk';
+
+    // Creating a temporary anchor element to trigger the download
+    const link = document.createElement('a');
+    link.href = resumePath;
+    link.download = 'Resume.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
