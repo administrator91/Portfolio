1dbb0669bdd473ded9377222e0d3a751dcbc54b1
Rajarshi Sarkar <143781304+administrator91@users.noreply.github.com>
2025-02-15 10:23:18 +0530

    Update sc.js

--- script/sc.js
+++ script/sc.js
@@ -1,6 +1,6 @@
 function downloadResume() {
     // path
-    const resumePath = 'https://drive.google.com/file/d/1Iusq1ipDRoW99417123AfkATk00X0C5L/view?usp=drivesdk';
+    const resumePath = 'https://drive.google.com/file/d/1kW8ADPyaxNi4nYQF7KwX-DA8xSvP35ZF/view?usp=drivesdk';
 
     // Creating a temporary anchor element to trigger the download
     const link = document.createElement('a');
