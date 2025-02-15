67924ce6d56ca047c0af9ded8d84fb3e543cc9de
Rajarshi Sarkar <143781304+administrator91@users.noreply.github.com>
2025-02-15 10:43:54 +0530

    Update sc.js

--- script/sc.js
+++ script/sc.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("downloadButton").addEventListener("click", function () {
-        var resumeUrl = "https://drive.google.com/file/d/1kW8ADPyaxNi4nYQF7KwX-DA8xSvP35ZF/view?usp=drivesdk";
+        var resumeUrl = "https://drive.google.com/file/d/1kW8ADPyaxNi4nYQF7KwX-DA8xSvP35ZF/view?usp=drivesdk"; // Replace with your actual File ID
 
         var a = document.createElement("a");
         a.href = resumeUrl;
-        a.download = "Rajarshi_Sarkar_Resume.pdf"; // Set file name
+        a.download = "Rajarshi_Sarkar_Resume.pdf"; // Set the desired file name
         document.body.appendChild(a);
         a.click();
         document.body.removeChild(a);
