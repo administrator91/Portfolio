c2f5dcff89673c9a28e6acbdc291bcde423ec900
Rajarshi Sarkar <143781304+administrator91@users.noreply.github.com>
2025-02-15 10:35:12 +0530

    Update sc.js

--- script/sc.js
+++ script/sc.js
@@ -1,12 +1,14 @@
-function downloadResume() {
-    // path
-    const resumePath = 'https://drive.google.com/file/d/1kW8ADPyaxNi4nYQF7KwX-DA8xSvP35ZF/view?usp=drivesdk';
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("downloadButton").addEventListener("click", function () {
+        // Replace 'YOUR_FILE_ID' with your actual Google Drive file ID
+        var resumeUrl = "https://drive.google.com/file/d/1kW8ADPyaxNi4nYQF7KwX-DA8xSvP35ZF/view?usp=drivesdk";
 
-    // Creating a temporary anchor element to trigger the download
-    const link = document.createElement('a');
-    link.href = resumePath;
-    link.download = 'Resume.pdf';
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-}
+        var a = document.createElement("a");
+        a.href = resumeUrl;
+        a.target = "_blank"; // Opens in a new tab (optional)
+        a.download = "Rajarshi_Sarkar_Resume.pdf"; // Set the desired file name
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+    });
+});
